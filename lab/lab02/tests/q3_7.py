9d9908a6bdb5454bb25f8d6308e10eecb51096e3
Taylor Gibson <taylorgibson@gmail.com>
2022-08-23 15:30:24 +0000

    Fixing tests for Fall 2022

--- lab/lab02/tests/q3_7.py
+++ lab/lab02/tests/q3_7.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q3_7',
     'points': None,
-    'suites': [   {   'cases': [{'code': ">>> farmers_markets_locations_by_latitude.first('y')\n64.86275", 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> farmers_markets_locations_by_latitude.first('y')\n64.862750000000005", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
