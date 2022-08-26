ed6ec6cd3ee20f42d2f349acd66f7bf05ef39a22
bb <beth.bumgardner@ncssm.edu>
2022-08-26 19:39:05 +0000

    hw 7,9 test updates

---
+++ hw/hw07/tests/q2_3.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_3', 'points': None, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ hw/hw07/tests/q2_6.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_6',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> cutoff_ten_percent in [1,2,3]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
