b68e2329cddc712701984aa74ca2c23db3393eac
bb <beth.bumgardner@ncssm.edu>
2022-08-26 19:10:42 +0000

    updated tests for labs

--- lab/lab06/tests/q1_8.py
+++ lab/lab06/tests/q1_8.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q1_8',
     'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> type(simulation_and_statistic(.5, model_proportions)) == float\nTrue', 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {'code': '>>> type(simulation_and_statistic(.5, model_proportions)) in [float, np.float64]\nTrue', 'hidden': False, 'locked': False},
                                    {'code': '>>> 0 <= simulation_and_statistic(.5, model_proportions) <= 25\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
--- lab/lab07/tests/q2_1.py
+++ lab/lab07/tests/q2_1.py
@@ -1,1 +1,8 @@
-test = {'name': 'q2_1', 'points': None, 'suites': [{'cases': [{'code': '>>> helicopters.num_rows\n130', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q2_1',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> helicopters.num_rows\n130', 'hidden': False, 'locked': False},
+                                   {'code': ">>> all(helicopters.column('Obstruction') == False)\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
