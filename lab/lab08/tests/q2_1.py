ce5a9be1922b5b94ef5af3db476157469e3d3be9
bb <beth.bumgardner@ncssm.edu>
2022-09-02 22:38:27 +0000

    lab08 test updated

--- lab/lab08/tests/q2_1.py
+++ lab/lab08/tests/q2_1.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q2_1',
     'points': None,
-    'suites': [   {   'cases': [{'code': ">>> np.random.seed(123)\n>>> one_sample_mean(salaries, 'salary', 100)\n63684.4517", 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> np.random.seed(123)\n>>> one_sample_mean(salaries, 'salary', 100)\n63684.451699999998", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
