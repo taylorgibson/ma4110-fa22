4db83e01404a443c8b6f6282468127257b9d6c78
bb <beth.bumgardner@ncssm.edu>
2022-09-19 15:33:14 +0000

    Lab 5 updates

---
+++ lab/lab05/tests/q2_1.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_1',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> longer_than_five == 35453\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab/lab05/tests/q2_3.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_3',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> 1000 <= total_score <= 10000\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
